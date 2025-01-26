7d15360d9e86579c708da59072d443c2740853e8
venanciogomani <venanciogomani@gmail.com>
2025-01-26 22:17:41 +0100

    products overview setup

---
+++ src/components/concepts/products/ProductsOverview.tsx
@@ -0,0 +1,12 @@
+export default function ProductsOverview() {
+  return (
+    <div className="flex flex-col h-full">
+      <h1 className="text-2xl text-bold text-slate-800">Products Overview</h1>
+      <div className="mt-4 bg-white rounded-lg w-full h-full">
+        <div className="p-4">
+          <h2 className="text-lg text-gray-800">Coming Soon...</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,16 @@
+import ProductsOverview from '../../components/concepts/products/ProductsOverview';
 import Header from '../../components/shared/Header/Header';
 import Sidebar from '../../components/shared/Sidebar/Sidebar';
 
 export default function Dashboard() {
   return (
-    <div className="flex justify-between w-screen bg-gray-100">
+    <div className="flex justify-between w-screen h-screen bg-gray-200">
       <Sidebar />
-      <div className="flex flex-col w-4/5 h-full">
+      <div className="flex flex-col w-4/5">
         <Header />
-        <div className="p-4">All pages and routes</div>
+        <div className="p-4 h-full">
+          <ProductsOverview />
+        </div>
       </div>
     </div>
   );
