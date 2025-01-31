14830d80d77fc39ce9a05f94b3f6457d2b4bbb9c
venanciogomani <venanciogomani@gmail.com>
2025-01-31 20:44:30 +0100

    add create button on products

--- src/components/concepts/products/ProductsOverview.tsx
+++ src/components/concepts/products/ProductsOverview.tsx
@@ -6,11 +6,15 @@ import { createProductsDataTable } from './controllers/ProductsController';
 import { createCategoriesDataTable } from './controllers/CategoriesController';
 import { createTagsDataTable } from './controllers/TagsController';
 import FilterDrawer from '../../shared/FilterDrawer/FilterDrawer';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductsOverview() {
   const [show, setShow] = useState<boolean>(false);
+  const [openCreate, setOpenCreate] = useState<boolean>(false);
   const [selectedTab, setSelectedTab] = useState<string>('products');
   const [tableData, setTableData] = useState<IDataItem>({} as IDataItem);
+  const navigate = useNavigate();
 
   const productsData = createProductsDataTable(products);
   const categoriesData = createCategoriesDataTable(categories);
@@ -20,11 +24,15 @@ export default function ProductsOverview() {
     if (Object.keys(productsData).length) {
       setTableData(productsData);
       setSelectedTab('products');
+      setShow(false);
+      setOpenCreate(false);
     }
   }, [products]);
 
   const handleSelectTab = (tab: string) => {
     setSelectedTab(tab);
+    setShow(false);
+    setOpenCreate(false);
     if (tab === 'products') {
       setTableData(productsData);
     } else if (tab === 'categories') {
@@ -90,17 +98,56 @@ export default function ProductsOverview() {
               </div>
               <div className="p-2 grow border-b border-slate-300">&nbsp;</div>
             </div>
-            {selectedTab === 'products' && (
-              <div className="relative p-4">
-                <TuneOutlinedIcon
-                  className="text-sky-600 cursor-pointer"
-                  onClick={() => setShow(true)}
-                />
-                <span className="absolute top-2 right-2 bg-pink-600 px-1 rounded-full text-white text-xs">
-                  2
-                </span>
+            <div className="flex items-center">
+              <div
+                className="relative flex items-center px-3 py-2 bg-sky-800 rounded
+                hover:bg-sky-600 transition duration-300 ease-in-out cursor-pointer"
+                onClick={() => setOpenCreate(!openCreate)}
+              >
+                <span className="text-white">Create</span>
+                <ExpandMoreOutlinedIcon className="ml-2 text-white" />
+                {openCreate && (
+                  <div className="absolute bg-white shadow-lg rounded flex flex-col top-12 right-0 min-w-40 border border-gray-200">
+                    <div
+                      className="w-full p-2 text-sm cursor-pointer bg-transparent 
+                      hover:bg-gray-100 transition duration-300 ease-in-out"
+                      onClick={() => navigate('/admin/products/create')}
+                    >
+                      Product
+                    </div>
+                    <div
+                      className="w-full p-2 text-sm cursor-pointer bg-transparent 
+                      hover:bg-gray-100 transition duration-300 ease-in-out"
+                      onClick={() => {
+                        console.log('open create modal');
+                      }}
+                    >
+                      Category
+                    </div>
+                    <div
+                      className="w-full p-2 text-sm cursor-pointer bg-transparent 
+                      hover:bg-gray-100 transition duration-300 ease-in-out"
+                      onClick={() => {
+                        console.log('open create modal');
+                      }}
+                    >
+                      Tag
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
+              {selectedTab === 'products' && (
+                <div className="relative p-4">
+                  <TuneOutlinedIcon
+                    className="text-sky-600 cursor-pointer"
+                    onClick={() => setShow(true)}
+                  />
+                  <span className="absolute top-2 right-2 bg-pink-600 px-1 rounded-full text-white text-xs">
+                    2
+                  </span>
+                </div>
+              )}
+            </div>
           </div>
           <DataTable columns={tableData.columns} rows={tableData.rows} />
         </div>
